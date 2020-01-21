7f7ac59e7497eac85f8be978260b7c54b4dedf68
Maja Gach <majaanna94@gmail.com>
2020-01-21 21:16:24 +0100

    added second js file

---
+++ showroom-cities.js
@@ -0,0 +1,26 @@
+//paris
+let buttonParis = document.getElementById("buttonParis");
+
+buttonParis.addEventListener("click", () => {
+  readMoreParis.style.display = "";
+  readMoreParis.textContent =
+    "A private show flat in the heart of the world’s design capital: Paris. Our wish is to offer you an intimate design experience attached to an authentic scenario. Most of all, we want to spend some quality time with you! A highly trained team is waiting to explain every detail of each brand, to tell the amazing stories behind the pieces and to answer all questions, in order to help to finish projects on time by showing the best choices.";
+});
+
+//berlin
+let buttonBerlin = document.getElementById("buttonBerlin");
+
+buttonBerlin.addEventListener("click", () => {
+  readMoreBerlin.style.display = "";
+  readMoreBerlin.textContent =
+    "A private show flat in the heart of the world’s design capital: Paris. Our wish is to offer you an intimate design experience attached to an authentic scenario. Most of all, we want to spend some quality time with you! A highly trained team is waiting to explain every detail of each brand, to tell the amazing stories behind the pieces and to answer all questions, in order to help to finish projects on time by showing the best choices.";
+});
+
+//london
+let buttonLondon = document.getElementById("buttonLondon");
+
+buttonLondon.addEventListener("click", () => {
+  readMoreLondon.style.display = "";
+  readMoreLondon.textContent =
+    "A private show flat in the heart of the world’s design capital: Paris. Our wish is to offer you an intimate design experience attached to an authentic scenario. Most of all, we want to spend some quality time with you! A highly trained team is waiting to explain every detail of each brand, to tell the amazing stories behind the pieces and to answer all questions, in order to help to finish projects on time by showing the best choices.";
+});
