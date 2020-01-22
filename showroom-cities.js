1882ce59bc21ee6694f27652eda343983872c325
Maja Gach <majaanna94@gmail.com>
2020-01-22 20:14:14 +0100

    showroom page pics responsive

--- showroom-cities.js
+++ showroom-cities.js
@@ -7,7 +7,7 @@ let buttonParis = document.getElementById("buttonParis");
 buttonParis.addEventListener("click", () => {
   readMoreParis.style.display = "";
   readMoreParis.textContent =
-    "Our wish is to offer you an intimate design experience attached to an authentic scenario. Conceived with different collections of furniture, lighting and accessories, the apartment and its professional staff are ready to inspire all our visitors with a private tour over amazing stories behind the pieces.";
+    "Our wish is to offer you an intimate design experience attached to an authentic scenario. The apartment and its professional staff are ready to inspire all our visitors with a private tour over amazing stories behind the pieces.";
 });
 
 //berlin
@@ -16,7 +16,7 @@ let buttonBerlin = document.getElementById("buttonBerlin");
 buttonBerlin.addEventListener("click", () => {
   readMoreBerlin.style.display = "";
   readMoreBerlin.textContent =
-    "A private show flat in the heart of the world’s design capital: Paris. Our wish is to offer you an intimate design experience attached to an authentic scenario. Most of all, we want to spend some quality time with you! A highly trained team is waiting to explain every detail of each brand, to tell the amazing stories behind the pieces and to answer all questions, in order to help to finish projects on time by showing the best choices.";
+    "A private show flat in the heart of the world’s design capital: Paris. Our wish is to offer you an intimate design experience attached to an authentic scenario. A highly trained team is waiting to explain every detail of each brand, to tell the amazing stories behind the pieces and to answer all questions.";
 });
 
 //london
